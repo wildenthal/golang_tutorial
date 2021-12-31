7734490db04747841e3460003a147c8116207439
wildenthal <8503469+wildenthal@users.noreply.github.com>
2021-12-31 18:22:54 +0000

    adds user db and dummy login page

--- src/lenslocked.com/controllers/users.go
+++ src/lenslocked.com/controllers/users.go
@@ -1,4 +1,5 @@
 package controllers
+
 import (
 	"fmt"
 	"net/http"
@@ -6,38 +7,82 @@ import (
 	"github.com/gorilla/schema"
 
 	"lenslocked.com/views"
+	"lenslocked.com/models"
 )
 
+// Users controller directs user information towards users model
+// for creation or authentication
 
 type Users struct {
-	NewView *views.View
+	NewView   *views.View
+	LoginView *views.View
+	us        *models.UserService
+}
+
+func NewUsers(us *models.UserService) *Users {
+	return &Users{
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+		us:        us,
+	}
 }
 
 type SignupForm struct {
+	Name string `schema:"name"`
 	Email string `schema:"email"`
 	Password string `schema:"password"`
 }
 
-func NewUsers() *Users {
-	return &Users{
-		NewView: views.NewView("bootstrap", "users/new"),
-	}
+type LoginForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
 }
 
+func parseForm(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+	dec := schema.NewDecoder()
+	if err := dec.Decode(dst, r.PostForm); err != nil {
+		return err
+	}
+	return nil
+}
+
+// New is a handler that renders the signup page
 func (u *Users) New(w http.ResponseWriter,r *http.Request) {
 	if err := u.NewView.Render(w,nil);err != nil {
 		panic(err)
 	}
 }
 
+// Create is used to process the signup form when a user enters
+// their name, email and password
+
 func (u *Users) Create(w http.ResponseWriter,r *http.Request) {
-	if err := r.ParseForm(); err != nil {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	dec := schema.NewDecoder()
-	form := SignupForm{}
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	user := models.User{
+		Name:     form.Name,
+		Email:    form.Email,
+		Password: form.Password,
+	}
+
+	if err := u.us.Create(&user); err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "User is", user)
+}
+
+// Login is used to process the login form when a user enters
+// their email and password
+
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, form)
 }
--- src/lenslocked.com/main.go
+++ src/lenslocked.com/main.go
@@ -5,21 +5,37 @@ import (
 	"net/http"
 
 	"lenslocked.com/controllers"
+	"lenslocked.com/models"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	// Config information
 	cfg := DefaultConfig()
-	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	psqlInfo := DefaultPostgresConfig().ConnectionInfo()
+	
+	// Connect to database
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
 
+	// Create controllers
+	staticC := controllers.NewStatic()
+	usersC := controllers.NewUsers(us)
+	
+	//Routing code
 	router := mux.NewRouter()
 
 	router.Handle("/", staticC.Home).Methods("GET")
 	router.Handle("/contact", staticC.Contact).Methods("GET")
 	router.HandleFunc("/signup", usersC.New).Methods("GET")
 	router.HandleFunc("/signup", usersC.Create).Methods("POST")
+	router.Handle("/login", usersC.LoginView).Methods("GET")
+	router.HandleFunc("/login",usersC.Login).Methods("POST")
 
 	http.ListenAndServe(fmt.Sprintf(":%d",cfg.Port), router)
 }
